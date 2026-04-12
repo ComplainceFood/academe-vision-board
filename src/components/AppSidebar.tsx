6738a19b0161aea300a7d482cd5e21bcc451dfba
ComplainceFood <reddym.fsc@gmail.com>
2026-04-12 17:17:06 -0500

    Update AppSidebar.tsx

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -148,7 +148,7 @@ export function AppSidebar() {
       {/* Header */}
       <SidebarHeader className="p-4 py-3 pb-3" style={{ borderBottom: "1px solid rgba(255,255,255,0.1)" }}>
         <div className="flex items-center px-2 py-1">
-          <SmartProfLogoWide height={78} />
+          <SmartProfLogoWide height={78} className="rounded-2xl" />
         </div>
       </SidebarHeader>
 
